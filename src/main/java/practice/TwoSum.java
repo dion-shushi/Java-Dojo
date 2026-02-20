3a7e372813043e182788e6c218da16dd10c75e1d
Shubha J <shubhajoshi21@gmail.com>
2026-02-20 01:29:13 +0000

    Initial Maven + JUnit Dojo setup

---
+++ src/main/java/practice/TwoSum.java
@@ -0,0 +1,17 @@
+package practice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+  // Returns indices of two numbers that add up to target (assumes one solution).
+  public int[] solve(int[] nums, int target) {
+    Map<Integer, Integer> seen = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+      int need = target - nums[i];
+      if (seen.containsKey(need)) return new int[]{seen.get(need), i};
+      seen.put(nums[i], i);
+    }
+    return new int[]{-1, -1};
+  }
+}
